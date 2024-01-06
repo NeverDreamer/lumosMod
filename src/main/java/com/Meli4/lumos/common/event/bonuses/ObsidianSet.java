46a7bc0d248d9f195c489628693908bee79c9167
Pelmeshkin <atn.nka@mail.ru>
2024-01-06 13:13:47 +0300

    Fix(Дебаффы получает ударивший носителя сета, поушн изменён на кровотечение) (Япиии)

--- src/main/java/com/Meli4/lumos/common/event/bonuses/ObsidianSet.java
+++ src/main/java/com/Meli4/lumos/common/event/bonuses/ObsidianSet.java
@@ -5,12 +5,12 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import shadows.apotheosis.ApotheosisObjects;
 
 @Mod.EventBusSubscriber
 public class ObsidianSet extends SetBonus {
@@ -32,11 +32,13 @@ public class ObsidianSet extends SetBonus {
         @SubscribeEvent
         public static void onLivingHurt(LivingHurtEvent event) {
 
-            if (event.getSource().getTrueSource() instanceof PlayerEntity) {
-                PlayerEntity player = (PlayerEntity) event.getSource().getTrueSource();
-                LivingEntity entity = event.getEntityLiving();
-                if (SetBonus.hasArmor(player, INSTANCE)) {
-                   entity.addPotionEffect(new EffectInstance(Effects.WITHER,entity instanceof PlayerEntity? 100 : 200,entity instanceof PlayerEntity? 2: 4));
+            if (event.getEntity() instanceof PlayerEntity) {
+                PlayerEntity player = (PlayerEntity) event.getEntity();
+                if(event.getSource().getImmediateSource() instanceof LivingEntity) {
+                    LivingEntity entity = (LivingEntity) event.getSource().getImmediateSource();
+                    if (SetBonus.hasArmor(player, INSTANCE)) {
+                        entity.addPotionEffect(new EffectInstance(ApotheosisObjects.BLEEDING, entity instanceof PlayerEntity ? 100 : 200, entity instanceof PlayerEntity ? 2 : 4));
+                    }
                 }
             }
         }
